427ac46e1080dbc9ac24b48eccffdd458b5cae23
Dinar Fakhriev <dinar.fahriev@yandex.ru>
2019-11-23 18:47:21 +0300

    Game Complexity with Cubes Number
    Добавление уровня сложности, с увеличением количества кубов каждые 1000 очков(максимум три раза).

---
+++ Assets/Scripts/GameComplexity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameComplexity : MonoBehaviour
+{
+    [SerializeField] private GameObject[] blocksPack = new GameObject[0];
+    [SerializeField] private TextMeshProUGUI scoreTMP;
+
+    private int complexityLevel = 1;
+    private float playerScore;
+
+    private void Update()
+    {
+        if (complexityLevel == 4)
+            return;
+
+        playerScore = System.Convert.ToInt32(scoreTMP.text);
+        if (playerScore < 1000 * complexityLevel)
+            return;
+        
+        ComplexityLevelIncrease();
+    }
+
+    private void ComplexityLevelIncrease()
+    {
+        blocksPack[complexityLevel - 1].SetActive(true);
+        complexityLevel++;
+    }
+}
